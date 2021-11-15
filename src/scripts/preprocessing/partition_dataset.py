be9b29deff85e87a6f591fdf8698b45ec6895cc9
sharath nair <returncode13@gmail.com>
2021-11-15 13:14:56 +0530

    changes to scripts

--- src/scripts/preprocessing/partition_dataset.py
+++ src/scripts/preprocessing/partition_dataset.py
@@ -20,47 +20,73 @@ import math
 import random
 
 
-def iterate_dir(source, dest, ratio, copy_xml):
+def iterate_dir(source, dest, train_percentage,val_test_ratio, copy_xml):
+    """
+    dataset is split as follows.
+    train_images contains train_percentage % of total images
+    the rest of the images is split into val and test images in ratio val:test  val_test_ratio
+    """
     source = source.replace('\\', '/')
     dest = dest.replace('\\', '/')
     train_dir = os.path.join(dest, 'train')
     test_dir = os.path.join(dest, 'test')
+    val_dir= os.path.join(dest,"val")
 
     if not os.path.exists(train_dir):
         os.makedirs(train_dir)
     if not os.path.exists(test_dir):
         os.makedirs(test_dir)
+    if not os.path.exists(val_dir):
+        os.makedirs(val_dir)
 
     images = [f for f in os.listdir(source)
               if re.search(r'([a-zA-Z0-9\s_\\.\-\(\):])+(?i)(.jpg|.jpeg|.png)$', f)]
 
     num_images = len(images)
-    num_test_images = math.ceil(ratio*num_images)
+    num_train_images=math.ceil(train_percentage*num_images)
 
-    for i in range(num_test_images):
+    rem_images=num_images-num_train_images
+    num_val_images = math.ceil(val_test_ratio*rem_images)
+
+    rem_images=rem_images-num_val_images
+    num_test_images =rem_images
+
+    print("distribute into {} train, {} val and {} test images".format(num_train_images,num_val_images,num_test_images))
+    for i in range(num_train_images):
         idx = random.randint(0, len(images)-1)
         filename = images[idx]
-        copyfile(os.path.join(source, filename),
-                 os.path.join(test_dir, filename))
-        if copy_xml:
-            xml_filename = os.path.splitext(filename)[0]+'.xml'
-            copyfile(os.path.join(source, xml_filename),
-                     os.path.join(test_dir,xml_filename))
-        images.remove(images[idx])
-
-    for filename in images:
         copyfile(os.path.join(source, filename),
                  os.path.join(train_dir, filename))
         if copy_xml:
             xml_filename = os.path.splitext(filename)[0]+'.xml'
             copyfile(os.path.join(source, xml_filename),
-                     os.path.join(train_dir, xml_filename))
+                     os.path.join(train_dir,xml_filename))
+        images.remove(images[idx])
+    
+    for i in range(num_val_images):
+        idx = random.randint(0, len(images)-1)
+        filename = images[idx]
+        copyfile(os.path.join(source, filename),
+                 os.path.join(val_dir, filename))
+        if copy_xml:
+            xml_filename = os.path.splitext(filename)[0]+'.xml'
+            copyfile(os.path.join(source, xml_filename),
+                     os.path.join(val_dir,xml_filename))
+        images.remove(images[idx])
+
+    for filename in images:
+        copyfile(os.path.join(source, filename),
+                 os.path.join(test_dir, filename))
+        if copy_xml:
+            xml_filename = os.path.splitext(filename)[0]+'.xml'
+            copyfile(os.path.join(source, xml_filename),
+                     os.path.join(test_dir, xml_filename))
 
 
 def main():
 
     # Initiate argument parser
-    parser = argparse.ArgumentParser(description="Partition dataset of images into training and testing sets",
+    parser = argparse.ArgumentParser(description="Partition dataset of images into training ,valuation and testing sets",
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
         '-i', '--imageDir',
@@ -76,9 +102,15 @@ def main():
         default=None
     )
     parser.add_argument(
-        '-r', '--ratio',
-        help='The ratio of the number of test images over the total number of images. The default is 0.1.',
-        default=0.1,
+        '-t', '--training_percentage',
+        help='The percentage of the number of train images over the total number of images. The default is 0.7.',
+        default=0.7,
+        type=float)
+
+    parser.add_argument(
+        '-r', '--val_test_ratio',
+        help='The ratio of the number of val images to the total number of images left after removing train_percentage of images The default is 0.7.',
+        default=0.7,
         type=float)
     parser.add_argument(
         '-x', '--xml',
@@ -91,7 +123,7 @@ def main():
         args.outputDir = args.imageDir
 
     # Now we are ready to start the iteration
-    iterate_dir(args.imageDir, args.outputDir, args.ratio, args.xml)
+    iterate_dir(args.imageDir, args.outputDir, args.training_percentage, args.val_test_ratio,args.xml)
 
 
 if __name__ == '__main__':
